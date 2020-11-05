18491ff2aec7db06b8522514bf8295cd75dacb38
mlgmontage <kettleofrassel1@gmail.com>
2020-11-05 11:22:39 +0500

    Enable CORS

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8080
 const volleyball = require("volleyball")
+const cors = require("cors")
 
 // Middlewares
 app.use(volleyball)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 // connection
 const knex = require("./connection")
