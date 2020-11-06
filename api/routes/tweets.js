fb0d95e081b7bbd44da1208e87f8486110df8090
mlgmontage <kettleofrassel1@gmail.com>
2020-11-06 15:56:36 +0500

    fixing schema && order by id

--- api/routes/tweets.js
+++ api/routes/tweets.js
@@ -4,7 +4,11 @@ const knex = require("../../connection")
 const tweetSchema = require('../schemas/tweet')
 
 router.get('/', async (req, res) => {
-  const tweets = await knex("Tweets").join('Users', 'Tweets.UserId', '=', 'Users.UserId').select('Tweets.TweetId', 'Tweets.Tweet', 'Users.name', 'Users.lastname', 'Users.login')
+  const tweets = await knex("Tweets")
+    .join('Users', 'Tweets.UserId', '=', 'Users.UserId')
+    .select('Tweets.TweetId', 'Tweets.Tweet', 'Users.name', 'Users.lastname', 'Users.login')
+    .orderBy('Tweets.TweetId', 'desc');
+
   res.json(tweets)
 })
 
--- api/schemas/tweet.js
+++ api/schemas/tweet.js
@@ -2,7 +2,7 @@ const Joi = require("joi")
 
 const schema = Joi.object({
   UserId: Joi.string().required(),
-  Tweet: Joi.string().min(5).max(256).required()
+  Tweet: Joi.string().min(5).required()
 })
 
 module.exports = schema
