1d2fd8fcbfdb5b874289cb59732d49162011559a
mlgmontage <kettleofrassel1@gmail.com>
2020-11-06 18:24:10 +0500

    useNullAsDefault

--- connection.js
+++ connection.js
@@ -2,7 +2,8 @@ const connection = require("knex")({
   client: 'sqlite3',
   connection: {
     filename: "./db/storage.sqlite"
-  }
+  },
+  useNullAsDefault: true
 })
 
 module.exports = connection
