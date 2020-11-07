978bd5df741e8bbb296593384a624f8803bf4b23
mlgmontage <kettleofrassel1@gmail.com>
2020-11-07 10:33:11 +0500

    generating jwt token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,47 +1,50 @@
-const express = require("express")
-const router = express.Router()
-const knex = require("../../connection")
-const registerSchema = require("../schemas/register")
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const knex = require("../../connection");
+const registerSchema = require("../schemas/register");
 
-router.get('/', async (req, res) => {
-  const users = await knex("Users")
-  res.json(users)
-})
+router.get("/", async (req, res) => {
+  const users = await knex("Users");
+  res.json(users);
+});
 
-router.post('/register', async (req, res) => {
-  const body = req.body
-  if(!registerSchema.validate(body).error) {
+router.post("/register", async (req, res) => {
+  const body = req.body;
+  if (!registerSchema.validate(body).error) {
+    const login = await knex("Users").where("login", "=", body.login);
 
-    const login = await knex("Users").where("login", "=", body.login)
-
-    if(!login.length) {
-
-      const inserted = await knex.insert([body]).into("Users")
+    if (!login.length) {
+      const inserted = await knex.insert([body]).into("Users");
 
       res.json({
-        message: "login created"
-      })
+        message: "login created",
+      });
     } else {
       res.json({
-        message: "login already exists"
-      })
+        message: "login already exists",
+      });
     }
   } else {
-    res.json(registerSchema.validate(body))
+    res.json(registerSchema.validate(body));
   }
-})
+});
 
-router.post('/login', async (req, res) => {
-  const body = req.body
-  const login = await knex("Users").where("login", '=', body.login)
+router.post("/login", async (req, res) => {
+  const body = req.body;
+  const login = await knex("Users").where("login", "=", body.login);
 
-  if(login.length > 0 && body.password == login[0].password) {
-    res.json(login)
+  if (login.length > 0 && body.password == login[0].password) {
+    const token = await jwt.sign(login[0], process.env.JWT_SECRET, {
+      expiresIn: "1800s",
+    });
+
+    res.json({ token });
   } else {
     res.json({
-      message: "Auth error"
-    })
+      message: "Auth error",
+    });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
